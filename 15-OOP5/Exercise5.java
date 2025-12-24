e9fd05e674dfb3ad28c16c88487c13aa89cd358d
kibo <kibo@Mac-mini-kibo.local>
2025-12-24 13:39:08 +0300

    15-OOP5_1

--- 15-OOP5/Exercise5.java
+++ 15-OOP5/Exercise5.java
@@ -4,15 +4,21 @@ import java.util.stream.IntStream;
 
 public class Exercise5 {
     private static final long START_TIME = System.currentTimeMillis();
+    static final String ERROR_NUMBER = "Could not parse a number. Please, try again";
+    static final String ERROR_AGE = "Incorrect input. Age <= 0";
+    static final String ERROR_PET = "Incorrect input. Unsupported pet type";
+    static final String DOG = "dog";
+    static final String CAT = "cat";
+    static final double THOUSAND = 1000.0;
+    static final int TIMEOUT = 100;
 
     static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
         int animalCount = readInt(input);
-        input.nextLine();
-        List<Animal> pets = IntStream.range(0, animalCount)// for от 0 до animalCount
-                .mapToObj(_ -> createPet(input))// для каждого i создаем животное
-                .filter(Optional::isPresent)// только не пустые
-                .map(Optional::get)// для не пустых используем get
+        List<Animal> pets = IntStream.range(0, animalCount) // for от 0 до animalCount
+                .mapToObj(_ -> createPet(input)) // для каждого i создаем животное
+                .filter(Optional::isPresent) // только не пустые
+                .map(Optional::get) // для не пустых используем get
                 .toList(); // собираем в список
         printResult(pets);
     }
@@ -21,9 +27,11 @@ public class Exercise5 {
         /* Проверка и получение вводимых значений (цифр)
         Количество животных и их возраст*/
         try {
-            return input.nextInt();
+            int count = input.nextInt();
+            input.nextLine();
+            return count;
         } catch (InputMismatchException e) {
-            System.out.println("Could not parse a number. Please, try again");
+            System.out.println(ERROR_NUMBER);
             input.nextLine();
             return readNumberRecursive(input);
         }
@@ -36,19 +44,18 @@ public class Exercise5 {
 
     public static Optional<Animal> createPet(Scanner input) {
         String animal = input.nextLine();
-        if (animal.equals("dog") || animal.equals("cat")) {
+        if (animal.equals(DOG) || animal.equals(CAT)) {
             String name = input.nextLine();
             int age = readInt(input);
-            input.nextLine();
             if (age <= 0) {
-                System.out.println("Incorrect input. Age <= 0");
+                System.out.println(ERROR_AGE);
                 return Optional.empty();
             }
-            return animal.equals("dog")
+            return animal.equals(DOG)
                     ? Optional.of(new Dog(name, age))
                     : Optional.of(new Cat(name, age));
         }
-        System.out.println("Incorrect input. Unsupported pet type");
+        System.out.println(ERROR_PET);
         return Optional.empty();
     }
 
@@ -63,18 +70,18 @@ public class Exercise5 {
                     double walkTime = pet.goToWalk();
                     synchronized (System.out) {
                         System.out.printf("%s, start time = %.2f, end time = %.2f%n",
-                                pet, startTime / 1000.0, walkTime + startTime / 1000.0);
+                                pet, startTime / THOUSAND, walkTime + startTime / THOUSAND);
                     }
                 } catch (InterruptedException e) {
                     synchronized (System.out) {
-                        System.out.println(pet + " - walk was interrupted");
+                        System.out.println("THREAD ERROR");
                     }
                 }
             });
             threads.add(thread);
             thread.start();
             if (i < pets.size() - 1) {
-                TimeUnit.MILLISECONDS.sleep(100);
+                TimeUnit.MILLISECONDS.sleep(TIMEOUT);
             }
         }
         for (Thread thread : threads) {
