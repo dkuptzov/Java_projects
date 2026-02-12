142bd99e7a3c1613a0b7fba70de747b95d0de054
kibo <kibo@Mac-mini-kibo.local>
2026-02-12 15:46:44 +0300

    99-Stepik

---
+++ 99-StepikTrain/Stepik/src/main/java/Task517.java
@@ -0,0 +1,44 @@
+import com.google.gson.Gson;
+
+import java.util.*;
+
+/*
+У вас есть переменная data, которая содержит входные пользовательские данные.
+data - список из элементов целых чисел.
+Напишите код, который сортирует числовые элементы списка data в порядке
+возрастания и записывает результат через запятую в переменную result.
+
+Sample Input:
+[1, 3, 2, 4, 5]
+Sample Output:
+1, 2, 3, 4, 5
+ */
+
+public class Task517 {
+    public static void main(String[] args) {
+        List<Integer> data = readInput();
+        String result = "";
+        if (data != null) {
+            Collections.sort(data);
+            List<String> strings = new ArrayList<>();
+            for (int i : data)
+                strings.add(Integer.toString(i));
+            result = String.join(", ", strings);
+        }
+        System.out.println(result);
+    }
+
+
+    public static List<Integer> readInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        if (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            Gson gson = new Gson();
+            Integer[] dataArray = gson.fromJson(input, Integer[].class);
+            return Arrays.asList(dataArray);
+        }
+        scanner.close();
+        return null;
+    }
+}
